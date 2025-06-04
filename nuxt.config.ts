5e5cfcc59f981a89cabea89741887bb4db380fb5
Mathias Eudeline <mathias.eudeline.ext@groupama-ra.fr>
2025-06-04 09:44:56 +0200

    feat: add needed modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
-  devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/test-utils']
+    compatibilityDate: '2025-05-15',
+    devtools: {enabled: true},
+    css: ['~/assets/scss/main.scss'],
+    modules: [
+        '@nuxt/eslint',
+        '@nuxt/test-utils',
+        'vuetify-nuxt-module',
+        '@nuxtjs/tailwindcss',
+        '@vueuse/nuxt',
+        '@pinia/nuxt'
+    ],
+    vuetify: {
+        moduleOptions: {
+            /* module specific options */
+        },
+        vuetifyOptions: {
+            /* vuetify options */
+        },
+        tailwindcss: {
+            // Options
+        }
+    }
 })
